53fb6bc97d823a7b3b6a691405c240853f4368fb
chrislopez93 <sinretel@gmail.com>
2023-07-19 00:22:24 -0400

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-function accion(){}
+function accion(){
+    const input = document.querySelector('.input').value;
+    if (input <= 576){
+        alert('Tu resolución pertenece a la medida xs')
+    }
+    else if (input <= 768){
+        alert('Tu resolución pertenece a la medida sm')
+    }
+    else if (input <= 962){
+        alert('Tu resolución pertenece a la medida md')
+    }
+    else if (input >= 992){
+        alert('Tu resolución pertenece a la medida xl')
+    }
+}
